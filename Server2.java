36badfb087ef6061cdea85c3f2603deab32d2042
Kevin McCauley <ix0774wu@metrostate.edu>
2018-09-30 18:48:46 -0500

    Add files via upload
    Just need to break up packet.

--- Server2.java
+++ Server2.java
@@ -8,8 +8,12 @@ import java.util.Date;
 public class Server2 {
 	private final static int PORT = 2001;
 	public static void main(String[] args) throws IOException {
-		//Take a file.  Needs to be changed for cmd input
-		String fileName = "C:\\Users\\o0kma\\eclipse-workspace\\Testing Area\\src\\doggo.jpg";
+		//Take a file.
+		String fileName;
+		if (args.length > 0) {
+			fileName = args[0];
+		}
+		else fileName = "didn'twork.jpeg";
 		File file = new File(fileName);
 		int fileSize = (int) file.length();
 		//Put it into a byte[]
@@ -18,14 +22,16 @@ public class Server2 {
 		fis.read(bytesArray);
 		fis.close();
 		try (DatagramSocket socket = new DatagramSocket(PORT)) {
-			System.out.println("Waiting...");
+			System.out.println("Waiting for Request...");
 			while (true) {
 				try {
 					DatagramPacket request = new DatagramPacket(new byte[1024], 1024);
 					socket.receive(request);
+					System.out.println("Request Received.");
 					
 					DatagramPacket response = new DatagramPacket(bytesArray, bytesArray.length, request.getAddress(), request.getPort());
 					socket.send(response);
+					System.out.println("File Sent");
 				} catch (IOException ex) {
 					ex.printStackTrace();
 				}
